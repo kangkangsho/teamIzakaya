27b0d0a48776798723b86ac609ba4d871137dd3f
kangkang <kangkang@DESKTOP-L8F4TJR>
2017-09-12 11:38:56 +0900

    aa

---
+++ teamIzakaya/src/main/java/com/in/global/employees/employeesVO.java
@@ -0,0 +1,21 @@
+package com.in.global.employees;
+
+public class employeesVO {
+	private int emplNo;
+	private String emplName;
+	public int getEmplNo() {
+		return emplNo;
+	}
+	public void setEmplNo(int emplNo) {
+		this.emplNo = emplNo;
+	}
+	public String getEmplName() {
+		return emplName;
+	}
+	public void setEmplName(String emplName) {
+		this.emplName = emplName;
+	}
+	
+	
+
+}
